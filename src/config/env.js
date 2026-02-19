1492546137fe85c715233e28ebff29bfeb63b5f8
aadixix <amansrivastav184@gmail.com>
2026-02-19 22:34:24 +0530

    fix env

--- src/config/env.js
+++ src/config/env.js
@@ -1,4 +1,9 @@
-require("dotenv-flow").config();
+const path = require("path");
+const dotenv = require("dotenv");
+
+// Explicitly load correct .env file
+const envFile = process.env.NODE_ENV === "production" ? ".env.production" : ".env";
+dotenv.config({ path: path.resolve(process.cwd(), envFile) });
 
 module.exports = {
   PORT: process.env.PORT || 5000,
